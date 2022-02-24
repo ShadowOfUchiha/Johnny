a354ad97c3c9f3fa2b7aa374c4545b7153e9ca33
johnny <johnnymihai33@gmail.com>
2022-02-24 15:45:13 +0100

    dsdsd

--- index.php
+++ index.php
@@ -4,11 +4,68 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Portfolio, Johnny, Johnny Magielse, Software, Software Development, Software Developer, Software Engineer">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
+    <title>Portfolio</title>
 </head>
 
 <body>
-    
+    <nav class="navbar bg-dark text-white">
+        <div class="container">
+            <ul class="me-auto">
+                <h3>Johnny Magielse</h3>
+                <!-- <a class="logo"><img src="#" alt="logo"></a> -->
+            </ul>
+            <ul>
+                <li class="my-2 mx-4"><a href="#">Link</a></li>
+                <li class="my-2 mx-4"><a href="#">Link</a></li>
+                <li class="my-2 mx-4"><a href="#">Link</a></li>
+                <li class="my-2 mx-4"><a href="#">Link</a></li>
+                <button class="ms-4">My face</button>
+            </ul>
+        </div>
+    </nav>
+
+    <section class="header bg-image">
+        <div class="container h-100">
+            <div class="row row-cols-lg-2 row-cols-1 h-100 text-white">
+                <div class="my-auto text-header">
+                    <h1>Heading about me..</h1>
+                    <p>Dean is talking weird shit at this moment.<br> 
+                    Spoiler alert its about death? It's 23:15 fucking night wednesday</p>
+                    <p class="text-yellow">Yes I changed my mind.<br> 
+                        <span class="text-orange">I am gonna use flex instead of cols and maybe tailwind</span></p>
+                    <button>Click me</button>
+                    <span class="mx-4">
+                    <button class="bg-orange">Help me</button>
+                </div>
+                <div class="my-auto text-end">
+                    <img class="img-fluid h-img-1" width="350px" src="./img/plants2.jpg">
+                    <img class="img-fluid h-img-2" width="300px" src="./img/plants1.jpg">
+                    <img class="img-fluid h-img-3" width="200px" src="./img/plants3.jpg">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="introduction bg-dark py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col text-center">
+                    <h2 class="text-white">Yo, me the power elite arrived!</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col">
+
+                </div>
+                <div class="col">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
 </body>
 </html>
